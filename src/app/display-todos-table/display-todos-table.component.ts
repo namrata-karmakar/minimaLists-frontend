190dbfe5a5d7e9112f927f746be3516cb53087f1
namrata-karmakar <namrata.krmkr@gmail.com>
2021-07-27 12:26:06 +0530

    impl deleteTodoById fn

--- src/app/display-todos-table/display-todos-table.component.ts
+++ src/app/display-todos-table/display-todos-table.component.ts
@@ -1,28 +1,38 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, ViewChild } from '@angular/core';
 import { TodosDataDto, TodosService } from '../services/todos.service';
+import { MatTable } from '@angular/material/table';
 
 @Component({
-  selector: 'app-display-todos-table',
-  templateUrl: './display-todos-table.component.html',
-  styleUrls: ['./display-todos-table.component.css']
+	selector: 'app-display-todos-table',
+	templateUrl: './display-todos-table.component.html',
+	styleUrls: ['./display-todos-table.component.css']
 })
 export class DisplayTodosTableComponent implements OnChanges {
 
-  @Input() randomNumber: number = 0;
+	columnsToDisplay: string[] = ['todos', 'status', 'editButton', 'deleteButton'];
+	@Input() randomNumber: number = 0;
+	@ViewChild(MatTable) table!: MatTable<any>
+	dataSource: TodosDataDto[] = [];
 
-  constructor(private todosService: TodosService) { }
+	constructor(private todosService: TodosService) { }
 
-  ngOnChanges(): void {
-    this.getTodos();
-  }
+	ngOnChanges(): void {
+		this.getTodos();
+		this.table.renderRows();
+	}
 
-  dataSource: TodosDataDto[] = [];
-  async getTodos() {
-    const userID: string | null = sessionStorage.getItem("userID");
-    console.log(userID);
-    this.dataSource = await this.todosService.getTodosByUserID(userID);
-    this.dataSource = this.dataSource.reverse();
-  }
+	async getTodos() {
+		const userID: string | null = sessionStorage.getItem("userID");
+		console.log(userID);
+		this.dataSource = await this.todosService.getTodosByUserId(userID);
+		this.table.renderRows();
+		this.dataSource = this.dataSource.reverse();
+	}
 
-  columnsToDisplay: string[] = ['todos', 'status', 'editButton', 'deleteButton'];
+	async deleteTodo(todo: any): Promise<void> {
+		const { _id, userID } = todo;
+		await this.todosService.deleteTodoById(_id);
+		this.dataSource = await this.todosService.getTodosByUserId(userID);
+		this.table.renderRows();
+	}
 }
