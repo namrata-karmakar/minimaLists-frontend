a19fa9caa4579bc4261622470967d1dd88be5dfe
namrata-karmakar <namrata.krmkr@gmail.com>
2021-07-31 16:45:51 +0530

    delete todo dialog comp created

---
+++ src/app/delete-todo-dialog/delete-todo-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-todo-dialog',
+  templateUrl: './delete-todo-dialog.component.html',
+  styleUrls: ['./delete-todo-dialog.component.css']
+})
+export class DeleteTodoDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
