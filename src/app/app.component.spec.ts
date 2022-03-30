06e1ad3759724e709ff22c594281e256b73ec7a0
namrata-karmakar <namrata.krmkr@gmail.com>
2022-03-30 12:27:02 +0530

    changes in project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'todo-angular-ui2'`, () => {
+    it(`should have as title 'minimaLists-frontend'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('todo-angular-ui2');
+        expect(app.title).toEqual('minimaLists-frontend');
     });
 
     it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
         fixture.detectChanges();
         const compiled = fixture.nativeElement;
         expect(compiled.querySelector('.content span').textContent).toContain(
-            'todo-angular-ui2 app is running!'
+            'minimaLists-frontend app is running!'
         );
     });
 });
